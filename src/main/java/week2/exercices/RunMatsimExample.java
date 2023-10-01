fc6afb530d2d6edbefe0d3db25cd6e8327e332bd
Nils Jensen <nils.jensen@inf.ethz.ch>
2023-10-01 17:06:04 +0200

    original commit

---
+++ src/main/java/week2/exercices/RunMatsimExample.java
@@ -0,0 +1,15 @@
+package week2.exercices;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+
+
+public class RunMatsimExample {
+
+	public static void main(String[] args) {
+		Config config = ConfigUtils.loadConfig(args[0]);
+		Controler controler = new Controler(config);	
+		controler.run();
+	}
+}
